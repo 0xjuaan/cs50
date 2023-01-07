26f9be168b6e1c10aeb90607c2b344a0972668bd
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-07 04:39:32 +0000

    Sat, Jan 7, 2023, 4:39 AM +00:00

--- agree.c
+++ agree.c
@@ -6,11 +6,11 @@ int main(void)
 
 char choice = get_char("Do you agree to the terms and conditions? ");
 
-if (choice == "y")
+if (choice == 'y')
 {
     printf("good\n");
 }
-else if (choice == "n")
+else if (choice == 'n')
 {
     printf("well u cant use the service then m8\n");
 }
@@ -18,7 +18,7 @@ else
 {
      printf("either 'y' or 'n' fam\n");
 }
-}
+
 
 
 
