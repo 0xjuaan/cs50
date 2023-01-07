88305161f8a8eb1fd225c0905fed0b47327c1eca
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-07 04:13:38 +0000

    Sat, Jan 7, 2023, 4:13 AM +00:00

--- conditionals.c
+++ conditionals.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+
+int x = get_int("What is x? ");
+int y = get_int("What is y? ");
+
+if (x<y){
+    printf("x is less than y fam!!\n");
+}
+else if (x>y){
+    printf("sheesh, x is greater than y...\n");
+}
+else{
+     printf("wow... x is equal to y\n");
+}
+}
