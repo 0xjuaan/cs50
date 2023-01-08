0cc9161524f05554aa6f3eb42050b7682ee5f2ea
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-08 10:59:08 +0000

    Sun, Jan 8, 2023, 10:59 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
-string name = get_string("What's your name? ");
-printf("hello, %s\n", name);
+    //Getting user input (name)
+    string name = get_string("What's your name? ");
+    //Printing that name that was given
+    printf("hello, %s\n", name);
 }
