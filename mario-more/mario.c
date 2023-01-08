8e5e7fd1763753452605d3180f118876c97eb655
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-08 11:50:46 +0000

    Sun, Jan 8, 2023, 11:50 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -22,6 +22,11 @@ int main(void)
         {
             for (int i=0; i<n; i++)
             {
+                for(int j=0; j<(height-1); j++ )
+                {
+                    
+
+                }
                 printf("#"); //RATATATA!!
             }
             printf(" "); //space
