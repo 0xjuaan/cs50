ecaf9438a2f901bb23aaa171d26fba72affe7266
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-08 13:40:58 +0000

    Sun, Jan 8, 2023, 1:40 PM +00:00

--- credit/testing.c
+++ credit/testing.c
@@ -3,6 +3,12 @@
 
 int main(void)
 {
-long d1 = 378282246310005 % 10;
-printf("%li", d1);
+long number = 378282246310005;
+long d1 = number % 10;
+printf("First digit: %li\n", d1);
+long d2 = (number-d1) % 10;
+printf("Second digit: %li\n", d2);
+long d3 = (number-d1-d2) % 10;
+printf("Second digit: %li\n", d2);
+
 }
