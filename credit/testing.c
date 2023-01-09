8db38f11f2aca157ce197aca6154c99d91272000
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-09 06:12:51 +0000

    Mon, Jan 9, 2023, 6:12 AM +00:00

--- credit/testing.c
+++ credit/testing.c
@@ -6,9 +6,15 @@ int main(void)
 long number = 378282246310505;
 long d1 = number % 10;
 printf("First digit: %li\n", d1);
-long d2 = (0.1*(number-d1)) % 10;
+long d2 = (long)(0.1*(number-d1)) % 10;
 printf("Second digit: %li\n", d2);
-long d3 = 0.1*(0.1*(number-d1)-d2) % 10;
+long d3 = (long) 0.1*(0.1*(number-d1)-d2) % 10;
 printf("Third digit: %li\n", d3);
+
+
+for (i=0; i<16, i++)
+{
+    
+}
 
 }
