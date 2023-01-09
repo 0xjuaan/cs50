8cdb2bc631d3a8bb3718a389ffee1ec889984076
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-09 06:01:25 +0000

    Mon, Jan 9, 2023, 6:01 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,10 +6,7 @@ bool validate(number)
 int main(void)
 {
     long number = get_long("Insert credit card number: ");
-    if (number<3400000000000)
-    {
-        printf("INVALID\n");
-    }
+    
     int d1 = number % 10
     printf("%i", d1)
 }
