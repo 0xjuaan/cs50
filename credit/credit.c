d0e5f23e3f72d6a086adaab9c669329da452530a
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-10 04:51:16 +0000

    Tue, Jan 10, 2023, 4:51 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool checksum(long number)
 
 int main(void)
 {
@@ -13,7 +14,16 @@ int main(void)
         n= n/10;
         i++;
     }
-    
 
+
+}
+
+bool checksum(long number)
+{
+    int d1 = number%10
+    for (a=0; a<n; a++)
+    {
+        
+    }
 }
 
