4b3d73e664b7a6e552ca3427e8770e0cb57994a9
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-10 14:59:21 +0000

    Tue, Jan 10, 2023, 2:59 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,36 +1,92 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int checksum(long number);
-
 int main(void)
 {
+
     long number = get_long("Insert credit card number: ");
 
-    int i=0; //Number of digits
+    int i = 0; //Number of digits
     long n = number;
     while(n>=1)
     {
-        n= n/10;
+        n  = n / 10;
         i++;
     }
-    checksum(number);
+    n = number;
+    // printf("size = %i\n", i);
 
-}
-
-int checksum(long number)
-{
-    long n = number;
+    int sum1 = 0;
+    int sum2 = 0;
     int d = n % 10;
-    for (int a=0; a<i; a++)
-    {
-        n = 0.1*(n-d);
-        d=n%10;
 
-        if (a%2>0)
+    for (int a=0; a < i; a++)
+    {
+        n = 0.1 * (n - d);
+        if (a % 2 != 0)
         {
-            printf("d");
+
+            sum1 += (2*d)%10 + 0.1*((2*d)-((2*d)%10));
+        }
+        else
+        {
+            sum2 += d;
+        }
+        d=n%10;
+    }
+
+    if ((sum1 + sum2)%10==0)
+    {
+       // printf("Approved Checksum g\n");
+        if (i==15)
+        {
+            if (number >= 370000000000000)
+            {
+                if (number <= 379999999999999)
+                {
+                    printf("AMEX\n");
+                }
+            }
+            else if (number >= 340000000000000)
+            {
+                if (number <= 349999999999999)
+                {
+                    printf("AMEX\n");
+                }
+            }
+
+        }
+        else if (i==13 && number <=4999999999999)
+        {
+            if (number >=4000000000000)
+            {
+                printf("VISA\n");
+            }
+
+        }
+        else if (i==16)
+        {
+            if (number >=5100000000000000)
+            {
+                if (number <= 5599999999999999)
+                {
+                    printf("Mastercard\n");
+                }
+            }
+            else if (number <=4999999999999999)
+            {
+                if (number >=4000000000000000)
+                {
+                    printf("VISA\n");
+                }
+            }
         }
     }
+    else
+    {
+        printf("INVALID\n");
+    }
+
 }
+
 
---
+++ credit/try1 (credit).c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int checksum(long number);
+
+int main(void)
+{
+    long number = get_long("Insert credit card number: ");
+
+    int i=0; //Number of digits
+    long n = number;
+    while(n>=1)
+    {
+        n= n/10;
+        i++;
+    }
+    checksum(number);
+
+}
+
+int checksum(long number)
+{
+    long n = number;
+    int d = n % 10;
+    for (int a=0; a<i; a++)
+    {
+        n = 0.1*(n-d);
+        d=n%10;
+
+        if (a%2>0)
+        {
+            printf("d");
+        }
+    }
+}
+
