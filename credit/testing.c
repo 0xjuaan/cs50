8881ebe9c17804bfc2959beee0bcc18c076a624b
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-10 04:59:34 +0000

    Tue, Jan 10, 2023, 4:59 AM +00:00

--- credit/testing.c
+++ credit/testing.c
@@ -3,18 +3,27 @@
 
 int main(void)
 {
-long number = 378282246310505;
-long d1 = number % 10;
-printf("First digit: %li\n", d1);
-long d2 = (long)(0.1*(number-d1)) % 10;
-printf("Second digit: %li\n", d2);
-long d3 = (long) 0.1*(0.1*(number-d1)-d2) % 10;
-printf("Third digit: %li\n", d3);
+    long number = get_long("Insert credit card number: ");
 
+    int i=0; //Number of digits
+    long n = number;
+    while(n>=1)
+    {
+        n= n/10;
+        i++;
+    }
+    int d = n % 10;
+    for (int a=0; a<i; a++)
+    {
+        n = 0.1*(n-d);
+        d=n%10;
 
-for (i=0; i<16, i++)
-{
-    
+        if (a%2>0)
+        {
+        printf("d");
+        }
+    }
+
 }
 
-}
+
