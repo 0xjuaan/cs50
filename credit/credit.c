58acf5e946ff48af71c17830b0961b60ffbc71fa
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-10 04:48:32 +0000

    Tue, Jan 10, 2023, 4:48 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,12 +6,14 @@ int main(void)
 {
     long number = get_long("Insert credit card number: ");
 
-    long n;
+    int i=0; //Number of digits
+    long n = number;
     while(n>=1)
     {
-       long n= number/10;
-       
-
+        n= n/10;
+        i++;
     }
+    
+
 }
 
