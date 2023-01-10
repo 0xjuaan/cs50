41c96f4cd143fb4245d60b6034fe4d84e7708c22
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-10 04:34:04 +0000

    Tue, Jan 10, 2023, 4:34 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,13 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-bool validate(number)
 
 int main(void)
 {
     long number = get_long("Insert credit card number: ");
-
-    int d1 = number % 10
-    printf("%i", d1)
+    int d1
 }
 
