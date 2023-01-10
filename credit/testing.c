f9df3a05916a973837d085b0e936f5b7b162f25a
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-10 05:51:14 +0000

    Tue, Jan 10, 2023, 5:51 AM +00:00

--- credit/testing.c
+++ credit/testing.c
@@ -14,7 +14,8 @@ int main(void)
     }
     n = number;
 
-    int sum1 = 0;
+    int sum1 = -1;
+    int sum2 =0;
     int d = n%10;
 
     for (int a=0; a < i; a++)
@@ -24,11 +25,18 @@ int main(void)
 
         if (a%2==0)
         {
-        printf("%i", d);
-        sum1 += (2*d)%10 + 0.1*((2*d)-((2*d)%10));
+            printf("%i", d);
+            sum1 += (2*d)%10 + 0.1*((2*d)-((2*d)%10));
+        }
+        else if (a%2!=0)
+        {
+            sum2 += d;
         }
     }
+
     printf("\n%i\n", sum1);
+    printf("%i", sum2);
+
 }
 
 
