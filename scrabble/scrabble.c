914d996000ee732c2a119e26e94fc09d5eb35eb4
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-14 09:50:43 +0000

    Sat, Jan 14, 2023, 9:50 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,31 +6,43 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int k;
-int compute_score(string words[k]);
+int compute_score(string word);
 
 int main(void)
 {
     // Get input words from both players
-    string words[2];
-    words[0] = get_string("Player 1: ");
-    words[1] = get_string("Player 2: ");
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
 
     // Score both words
-    int score0 = compute_score(words[0]);
-    int score1 = compute_score(words[1]);
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    if (score1>score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1<score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("DRAW!\n");
+    }
 
     // TODO: Print the winner
 }
-int compute_score(string words[k])
+int compute_score(string word)
 {
     // TODO: Compute and return score for string
     int n=0;
 
-    for (int i = 0; i<strlen(words[k]); i++)
+    for (int i = 0; i<strlen(word); i++)
     {
-        if (isalpha(words[k][i]))
+        if (isalpha(word[i]))
         {
-            int n = tolower(words[k][i]);
+            n = tolower(word[i]);
             n += POINTS[n-97];
         }
         else
@@ -39,6 +51,5 @@ int compute_score(string words[k])
         }
 
     }
-
-
+    return n;
 }
