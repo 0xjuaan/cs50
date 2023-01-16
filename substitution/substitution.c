b1af50143a5f3df9fee5adde7648d206fdae9ef8
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-16 06:33:34 +0000

    Mon, Jan 16, 2023, 6:33 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,7 +6,8 @@
 int main(int argc, string argv[])
 {
 
-    char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+    char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
     if (argc == 2)
     {
         string plaintext = get_string("Plaintext: ");
