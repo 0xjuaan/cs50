cfb956b6b162b063bee2d46ca2895858bdb5db7d
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-16 06:26:27 +0000

    Mon, Jan 16, 2023, 6:26 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,10 +3,13 @@
 
 int main(int argc, string argv[])
 {
+
+    char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
     if (argc == 2)
     {
         string plaintext = get_string("Plaintext: ");
-        
+
+        for (int i=0;)
         return 0;
     }
     else
