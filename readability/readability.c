282692898125e05705d3b2e17be2045b98e4432b
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-16 04:57:57 +0000

    Mon, Jan 16, 2023, 4:57 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int sentences, words, letters
+int sentences, words, letters = 0;
+
 int main(void)
 {
     string text = get_string("Text: ");
 
-    for (int i=0; i < strlen(text))
+    for (int i=0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+    }
 
 }
