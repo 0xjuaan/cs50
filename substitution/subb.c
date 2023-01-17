cd4c8f7d7570c2bb7fff237866d80211d2c87e5a
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-17 10:30:24 +0000

    Tue, Jan 17, 2023, 10:30 AM +00:00

--- substitution/subb.c
+++ substitution/subb.c
@@ -11,51 +11,9 @@ int main(int argc, string argv[])
     char alphabet[] = "abcdefghijklmnopqrstuvwxyz"
 }
 
- bool argcheck(string key, int argc, string alphabet)
+bool argcheck(string argv[], int argc, string alphabet)
 {
-    int c = 0;
 
-    if (argc==2)
-    {
-        if (strlen(key)==26)
-        {
-            wemadeit = true;
-            for (int a = 0; a < 26; a++)
-            {
-                for (int b = 0; b < 26; b++)
-                {
-                    if (key[a] == alphabet[b])
-                    {
-                        c++;
-                        break;
-                        printf("%i", c);
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-                break;
-            }
-            if (c == 26)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return false;
-    }
 
 
 
