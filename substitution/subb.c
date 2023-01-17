3d4f8467de9b5a85c962bad283b44bab21bb89cd
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-17 10:39:34 +0000

    Tue, Jan 17, 2023, 10:39 AM +00:00

--- substitution/subb.c
+++ substitution/subb.c
@@ -3,17 +3,61 @@
 #include <string.h>
 #include <ctype.h>
 bool wemadeit;
-bool argcheck(string key, int argc, string alphabet);
+bool argcheck(string argv[], int argc, string alphabet);
 
 
 int main(int argc, string argv[])
 {
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz"
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+    argcheck(argv[], argc, alphabet);
+
+    if (argcheck(argv[], argc, alphabet)==true)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
 }
 
 bool argcheck(string argv[], int argc, string alphabet)
 {
-    
+    int c = 0;
+    if (argc!=2)
+    {
+        return false;
+    }
+    else if (strlen(argv[1])!=26)
+    {
+        return false;
+    }
+    for (int a = 0; a < 26; a++)
+    {
+        for (int b = 0; b < 26; b++)
+        {
+            if (argv[1][a] == alphabet[b])
+            {
+                c++;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    if (c==26)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+
 
 
 
