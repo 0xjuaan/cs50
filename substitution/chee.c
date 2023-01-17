8ace186cc39208e99a54a2e97cb837cc230912ed
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-17 06:13:04 +0000

    Tue, Jan 17, 2023, 6:13 AM +00:00

---
+++ substitution/chee.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+    char key[]= "VcHpRzGjNtLsKfBdQwAxEuYmOi";
+    qsort(key);
+    printf("%s", key);
+    return 0;
+}
