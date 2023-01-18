d2ac753bf89bae5de314a8ad5d7500e73baa6332
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-18 06:21:32 +0000

    Wed, Jan 18, 2023, 6:21 AM +00:00

--- wordle/word.c
+++ wordle/word.c
@@ -96,8 +96,7 @@ int main(int argc, string argv[])
         // array to hold guess status, initially set to zero
         int status[wordsize];
 
-        // set all elements of status array initially to 0, aka WRONG
-        // TODO #4
+    
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
 
