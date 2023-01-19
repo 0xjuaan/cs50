bce25f68478471d009641d844f009396296ea7b0
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-19 14:47:32 +0000

    Thu, Jan 19, 2023, 2:47 PM +00:00

--- factorial.c
+++ factorial.c
@@ -1,25 +1,25 @@
  #include <cs50.h>
  #include <stdio.h>
 
-void factorial(int n);
+int factorial(int n);
 
 int main(void)
 {
 
 int number = get_int("Number: ");
 
-factorial(number);
+printf("%i\n", factorial(number));
 
 
 }
 
 
-void factorial(int n)
+int factorial(int n)
 {
-    if (n = 1)
+    if (n == 1)
     {
         return;
     }
-    n * factorial(n-1)
+    return n * factorial(n-1);
 
 }
