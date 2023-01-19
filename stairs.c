7c5c284699823f8a9d29bd8f1c1579d33da3c19b
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-19 13:28:51 +0000

    Thu, Jan 19, 2023, 1:28 PM +00:00

--- stairs.c
+++ stairs.c
@@ -3,16 +3,22 @@
 #include <cs50.h>
 #include <ctype.h>
 
+void draw(int height);
+
+
 int main(void)
 {
     int height = get_int("Height: ");
-    for (int i = 0; i < height; i++)
+    draw(height);
+}
+void draw(int height)
+{
+for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < i+1; j++)
         {
             printf("#");
         }
             printf("\n");
-
     }
 }
