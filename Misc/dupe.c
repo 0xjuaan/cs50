fba0c5aad13b541fe49c7e6750ef438921ed5664
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-20 07:08:25 +0000

    Fri, Jan 20, 2023, 7:08 AM +00:00

--- Misc/dupe.c
+++ Misc/dupe.c
@@ -6,14 +6,26 @@
 
 int main(void)
 {
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    string sub      = "ABCDEFGGIJKLMNOPQRSTUVWXYZ"
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string sub      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     int array[26];
 
     for (int i = 0; i < 26; i++)
     {
-        
+        for (int j = 1; j++)
+        {
+        if (sub[i]==sub[j])
+        {
+            printf("Dupe\n");
+            return 1;
+        }
+        else
+        {
+            continue;
+        }
+        }
     }
+    printf("clean\n");
 
 }
