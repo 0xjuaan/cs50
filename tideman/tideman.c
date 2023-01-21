d4ba76e691e1d28209907cbb4fa6891c8d44f755
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-21 09:40:17 +0000

    Sat, Jan 21, 2023, 9:40 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -107,6 +107,7 @@ bool vote(int rank, string name, int ranks[])
         if (strcmp(name, candidates[i]) == 0)
         {
             ranks[i+1] = i+1;
+            return true;
         }
         else
         {
