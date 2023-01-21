08c256fd8680168fa08ec936854f2333f54a5e0c
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-21 05:49:02 +0000

    Sat, Jan 21, 2023, 5:49 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,10 +68,14 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count ; i++)
     {
-        
+
         if (strcmp(name, candidates[i].name) == 0)
         {
             candidates[i].votes ++;
+            return true;
+        }
+        else
+        {
             continue;
         }
     }
@@ -81,6 +85,6 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-
+    
     return;
 }
