3c778b4d12335f34447b4a543f32694e80765939
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-24 06:37:44 +0000

    Tue, Jan 24, 2023, 6:37 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -139,6 +139,10 @@ void record_preferences(int ranks[])
             }
         }
     }
+    for (int i = 0; i < candidate_count; i++)
+    for (int j = 0; j < candidate_count; j++)
+        printf("%i ", preferences[i][j]);
+
     return;
 }
 
