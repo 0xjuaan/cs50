14f219a65b82cb76a173afd0706987c340bbbe3a
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-24 05:26:09 +0000

    Tue, Jan 24, 2023, 5:26 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -123,11 +123,19 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int b = 0; b < candidate_count; b++)   //Iterate over the ranks[]
+    for (int b = 0; b < candidate_count; b++)   //Iterate over the candidates
     {
-        for (int c = 0; c < candidate_count; c++)
+        for (int c = 0; c < candidate_count; c++)   //Iterate over the candidates
         {
-            break;
+            if (pref[c] < pref[b])
+            {
+                preferences[c][b] = 1;
+                continue;
+            }
+            else
+            {
+                continue;
+            }
         }
     }
     return;
