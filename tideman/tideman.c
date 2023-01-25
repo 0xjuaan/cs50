e90ea8ad2cbb57ad5eb0e8bc1cc45ad4b39cba2a
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-25 05:29:47 +0000

    Wed, Jan 25, 2023, 5:29 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -176,7 +176,10 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    
+    void check_cycle(pair pairs[])
+    {
+        
+    }
 }
 
 // Print the winner of the election
