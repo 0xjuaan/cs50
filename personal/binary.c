fd2644e2b1cbc0198b61e7f03bbc6149b8c2f682
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-25 15:22:15 +0000

    Wed, Jan 25, 2023, 3:22 PM +00:00

--- personal/binary.c
+++ personal/binary.c
@@ -4,10 +4,12 @@
 int main(void)
 {
     int numbers[] = {1, 4, 7, 9, 16, 56, 70};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int search = get_int("Search for: ");
 }
 
 
-bool bsearch (int array[])
+bool bsearch (int array[], int size_, int search_)
 {
     
 }
