95bdb76cf5d0627e358d132dbb8c17fffec823cc
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-25 16:20:34 +0000

    Wed, Jan 25, 2023, 4:20 PM +00:00

--- personal/binary.c
+++ personal/binary.c
@@ -8,6 +8,15 @@ int main(void)
     int numbers[] = {1, 4, 7, 9, 16, 56, 70};
     int size = sizeof(numbers) / sizeof(numbers[0]);
     int search = get_int("Search for: ");
+
+    if (bsearch(numbers, size, search))
+    {
+        printf("Found!");
+    }
+    else
+    {
+        printf("Not found!");
+    }
 }
 
 
@@ -15,11 +24,11 @@ bool bsearch (int array[], int size, int search)
 {
     if (numbers[size/2] == search)
     {
-        printf("Found!");
+        return true;
     }
     else if (numbers[size/2] > search)
     {
-        for (int i = 0; i < size/2; i++)
+        for (int i = size/2 + 1; i < size; i++)
         {
             numbers[i] = '\0';
         }
@@ -31,6 +40,6 @@ bool bsearch (int array[], int size, int search)
         {
             numbers[i] = '\0';
         }
-        bsearch(numbers,, search)
+        bsearch(numbers, size/2 , search)
     }
 }
