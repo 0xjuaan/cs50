9cd7b42d8c2de7bb91f858bddffa3c334ef93102
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-25 08:38:18 +0000

    Wed, Jan 25, 2023, 8:38 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -178,7 +178,7 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     n = 0;
-    check_cycle(pairs);
+    check_cycle(pairs[n]);
 
 }
 
@@ -208,7 +208,7 @@ void print_winner(void)
     }
     return;
 }
-void check_cycle(pair duo[])
+void check_cycle(pair duo[n])
     {
         if (duo[n].loser == duo[n+1].winner && n + 1 == pair_count - 1)
         {
