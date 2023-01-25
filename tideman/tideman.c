96c9f6533b00984abeb2488cb3446c132a4314ca
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-01-25 08:49:02 +0000

    Wed, Jan 25, 2023, 8:49 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -34,7 +34,7 @@ void record_preferences(int ranks[]);
 void add_pairs(void);
 void sort_pairs(void);
 void lock_pairs(void);
-void check_cycle(pair duo[int a]);
+void check_cycle(pair duo[n]);
 void print_winner(void);
 
 int main(int argc, string argv[])
@@ -178,7 +178,7 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     n = 0;
-    check_cycle(pairs[0]);
+    check_cycle(&pairs[n]);
 
 }
 
@@ -208,18 +208,18 @@ void print_winner(void)
     }
     return;
 }
-void check_cycle(pair duo[int a])
+void check_cycle(pair duo[n])
 {
-    if (duo[a].loser == duo[a+1].winner && a + 1 == pair_count - 1)
+    if (duo[n].loser == duo[n+1].winner && n + 1 == pair_count - 1)
     {
-        locked[duo[a+1].winner][duo[a+1].loser] = false;
+        locked[duo[n+1].winner][duo[n+1].loser] = false;
     }
-    else if (duo[a].loser == duo[a+1].winner)
-    {            check_cycle(duo[a+1])
+    else if (duo[n].loser == duo[n+1].winner)
+    {            check_cycle(&duo[n+1]);
 
     }
     else
     {
-        locked[duo[a].winner][duo[a].loser] = true;
+        locked[duo[n].winner][duo[n].loser] = true;
     }
 }
