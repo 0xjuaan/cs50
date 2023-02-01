3ed69b6e299273a07bfffbd1841f303f3a0521ca
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-01 08:26:24 +0000

    Wed, Feb 1, 2023, 4:26 PM +08:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,5 +3,18 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    
+
+    for (int i = 0; i < height; i++) //Iterate vertically
+    {
+        for (int j = 0; j < height; j++) //Iterate horizontally
+        {
+            if (image[i][j] == 0)
+            {
+                image[i][j].Red = 66;
+                image[i][j].Green = ;
+                image[i][j].Blue = ;
+
+            }
+        }
+    }
 }
