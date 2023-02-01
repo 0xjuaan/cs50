585f4fe2584e42b6a8fdfec374483fb025de1671
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-01 09:10:09 +0000

    Wed, Feb 1, 2023, 5:10 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -4,6 +4,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++) //Iterate vertically
+    {
+        for (int j = 0; j < width; j++) //Iterate horizontally
+        {
+            int avg = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
     return;
 }
 
