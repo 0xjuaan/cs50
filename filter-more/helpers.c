77a79f77f964cdc0275aac3abc5d3d74922144cb
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-01 10:10:35 +0000

    Wed, Feb 1, 2023, 6:10 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -50,7 +50,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
                 else
                 {
-                    image[i][j].rgbtRed = (image[i+1][j].rgbtRed + image[i][j+1].rgbtRed) / 2;
+                    image[i][j].rgbtRed = (image[i+1][j].rgbtRed + image[i][j+1].rgbtRed + ) / 2;
                     image[i][j].rgbtGreen = (image[i+1][j].rgbtGreen + image[i][j+1].rgbtGreen) / 2;
                     image[i][j].rgbtBlue = (image[i+1][j].rgbtBlue + image[i][j+1].rgbtBlue) / 2;
                 }
