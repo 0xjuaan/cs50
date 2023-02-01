93e582d7cc92cfc845b5af76a6ac122e2b697fc5
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-01 06:17:26 +0000

    Wed, Feb 1, 2023, 2:17 PM +08:00

--- license/license.c
+++ license/license.c
@@ -32,10 +32,12 @@ int main(int argc, char *argv[])
         strcpy(plates[idx], buffer);
         idx++;
 
+
     }
 
     for (int i = 0; i < 8; i++)
     {
         printf("%s\n", plates[i]);
     }
+    free(plates)
 }
