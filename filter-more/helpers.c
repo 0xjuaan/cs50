afba10a4169bf5806a9a049a9746e4e5e9247118
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-03 08:51:50 +0000

    Fri, Feb 3, 2023, 4:51 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -85,5 +85,48 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[height][width];
+    for (int a = 0; a < height; a++) //Copying the image to a temp
+    {
+        for (int b = 0; b < width; b++)
+        {
+            temp[a][b] = image[a][b];
+        }
+    }
+
+    for (int i = 0; i < height; i++) //Iterate vertically
+    {
+        for (int j = 0; j < width; j++) //Iterate horizontally
+        {
+            int sumr = 0;
+            int sumg = 0;
+            int sumb = 0;
+            int n = 0;
+
+            for (int k = i-1; k < i+2; k++) //vertical iteration of 3x3
+            {
+                for (int l = j-1; l < j+2; l++) //horizontal iteration of 3x3
+                {
+                    if (l < 0 || l >= width)
+                    {
+                        continue;
+                    }
+                    else if (k < 0 || k >= height)
+                    {
+                        continue;
+                    }
+                    sumr += temp[k][l].rgbtRed;
+                    sumg += temp[k][l].rgbtGreen;
+                    sumb += temp[k][l].rgbtBlue;
+                    n++;
+                }
+            }
+
+            image[i][j].rgbtRed = round((float) sumr / n);
+            image[i][j].rgbtGreen = round((float) sumg / n);
+            image[i][j].rgbtBlue = round((float) sumb / n);
+
+        }
+    }
     return;
 }
