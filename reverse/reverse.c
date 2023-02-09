e4f67a485cd6c50cd15cc716ce015dab55128482
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-09 14:04:40 +0000

    Thu, Feb 9, 2023, 10:04 PM +08:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
         data2[i] = data[dataSize-1-i];
     }
 
-    fseek(input, -dataSize, SEEK_END); //Setting cursor after header
+    fseek(output, sizeof(WAVHEADER), SEEK_SET); //Setting cursor after header
 
     fwrite(&data2, sizeof(BYTE), dataSize, output);
 
