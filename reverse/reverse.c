64a6fbdd8d420ea30c80ba390c676e1a7aa49f39
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-10 04:12:35 +0000

    Fri, Feb 10, 2023, 12:12 PM +08:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -57,22 +57,22 @@ int main(int argc, char *argv[])
     int blockSize = get_block_size(header);
     int dataSize = header.subchunk2Size;
 
-    BYTE data[dataSize];
+    BYTE data[dataSize/blockSize];
 
     fseek(input, sizeof(WAVHEADER), SEEK_SET); //Setting cursor after header
 
     fread(&data, blockSize, dataSize/blockSize, input);
 
-    BYTE data2[dataSize]; //Creating the reversed array
+    BYTE data2[dataSize/blockSize]; //Creating the reversed array
 
-    for (int i = 0; i < dataSize; i++) //Array reversal
+    for (int i = 0; i < dataSize/blockSize; i++) //Array reversal
     {
         data2[i] = data[dataSize-1-i];
     }
 
-    fseek(output, sizeof(WAVHEADER)+1, SEEK_SET); //Setting cursor after header
+    fseek(output, sizeof(WAVHEADER), SEEK_SET); //Setting cursor after header
 
-    fwrite(&data2, sizeof(BYTE), dataSize, output);
+    fwrite(&data2, blockSize, dataSize/blockSize, output);
 
 
     fclose(input);
