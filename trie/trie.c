8bde11a5dc1801e101dab6e278dffc49da7f7ae7
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-12 03:41:34 +0000

    Sun, Feb 12, 2023, 11:41 AM +08:00

--- trie/trie.c
+++ trie/trie.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
     }
 
     // Add words to the trie
-    while (fscanf(infile, "%s", name) == 1) 
+    while (fscanf(infile, "%s", name) == 1)
     {
         node *cursor = root;
 
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
                 cursor->children[index] = new;
             }
 
-            // Go to node which we may have just been made
+            // Go to node which may have just been made
             cursor = cursor->children[index];
         }
 
@@ -125,7 +125,7 @@ bool unload(void)
 
 void unloader(node* current)
 {
-    
+
     // Iterate over all the children to see if they point to anything and go
     // there if they do point
     for (int i = 0; i < SIZE_OF_ALPHABET; i++)
