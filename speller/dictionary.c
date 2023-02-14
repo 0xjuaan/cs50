0ceff11559fd7ba9af23f9c65a01efe1fa4010dc
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-14 10:43:08 +0000

    Tue, Feb 14, 2023, 6:43 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -30,7 +30,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    FILE *dict = fopen(dictionaries/large);
+    FILE *dict = fopen(large, "r");
 
     if (dict == NULL)
     {
