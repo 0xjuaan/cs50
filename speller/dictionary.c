26012d728346099dc561c16a2c293fad6e1b6c87
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-16 04:10:40 +0000

    Thu, Feb 16, 2023, 12:10 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -53,10 +53,6 @@ bool load(const char *dictionary)
         }
 
     }
-
-
-
-
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
@@ -140,7 +136,7 @@ bool lcheck (struct node* word_hashed)
 void add(struct node* node, char* word_dict)
 {
     node->next = malloc(sizeof(node));
-    
+
     if (node->next->has_word == false)
     {
         strcpy(node->next->word,word_dict);
