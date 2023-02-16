07f9b9ff3ac649c76305848f1b8eae5b2c062c0d
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-16 05:20:31 +0000

    Thu, Feb 16, 2023, 1:20 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -41,7 +41,9 @@ bool load(const char *dictionary)
             buffer[strcspn(buffer, "\n")] = 0;
             v = hash(buffer);
 
-            if (table[v]->has_word == false)
+            table[v]->next = malloc(sizeof(struct node));
+
+            if (table[v]->next->has_word == false)
             {
                 strcpy(table[v]->word, buffer);
             }
