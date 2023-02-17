b7b3a0195b5a23e024976aef7bc639883e791a0a
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-17 06:21:34 +0000

    Fri, Feb 17, 2023, 2:21 PM +08:00

--- euler/problem2.c
+++ euler/problem2.c
@@ -6,11 +6,16 @@ double fibsum(double *n1, double *n2);
 
 int main(void)
 {
-    double n1 = 1;
-    double n2 = 2;
+    double *n1;
+    double *n2;
+
+    *n1 = 1;
+    *n2 = 2;
+
+    double n = fibsum(n1, n2);
 }
 
-double fibsum(double)
+double fibsum(double *n1, double* n2)
 {
-
+    return fibsum(n1, n2);
 }
