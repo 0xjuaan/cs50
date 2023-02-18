0403803498bfd60b2088fecd7ecb633ee8688339
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-18 14:25:03 +0000

    Sat, Feb 18, 2023, 10:25 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -122,7 +122,7 @@ bool check(const char *word)
     {
         return true;
     }
-    else if (table[v]->next != NULL && table[v]->next->has_word)
+    else if (table[v]->next->has_word)
     {
         return lcheck(table[v], word); //If not the 1st word, go forth in the list and return that result
     }
