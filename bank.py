1edb2dad6b2fa9600745878bb832833cf199b41b
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-21 03:26:34 +0000

    Tue, Feb 21, 2023, 11:26 AM +08:00

--- bank.py
+++ bank.py
@@ -2,10 +2,10 @@
 greeting = input("Greeting: ")
 
 if greeting.lower() == "hello":
-     x = 100
+     x = 0
 elif greeting[0].lower() == "h":
      x = 20
 else:
-     x = 0
+     x = 100
 
 print("$" + str(x))
