5fafaa9afe9c5e148de7ad24cad037b00de81778
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-22 12:07:57 +0000

    Wed, Feb 22, 2023, 8:07 PM +08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -56,7 +56,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    
+    teams.split()
 
 
 if __name__ == "__main__":
