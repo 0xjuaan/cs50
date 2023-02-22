71e27d15cc22305513e11ee3583608987659208b
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-22 11:27:56 +0000

    Wed, Feb 22, 2023, 7:27 PM +08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -22,6 +22,9 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    for i in range(N):
+        simulate_tournament(teams)
+        
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
