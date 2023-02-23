59e9524f22c8351fea91f6a5c0b2f867435ca29e
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-23 14:37:00 +0000

    Thu, Feb 23, 2023, 10:37 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -14,6 +14,7 @@ def main():
     # Calculating Checksum
     sum1 = []
     sum2 = 0
+    sum3 = 0
     for i in range(len_number):
 
         if i % 2 == 0:
@@ -22,6 +23,23 @@ def main():
             sum1.append(2 * number % 10)
 
         number = (number - number % 10) / 10
+
+    for sum in sum1:
+        if sum < 10:
+            sum3 += sum
+        else:
+            sum3 += (sum - sum % 10) / 10 + sum % 10
+
+    sum = sum2 + sum3
+
+    if sum % 10 == 0:
+        checksum = True
+    else:
+        checksum = False
+        print("INVALID")
+        exit(1)
+
+
 
 
 
