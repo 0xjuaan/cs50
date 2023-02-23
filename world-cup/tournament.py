a459eaec72126afc937d29d963d302be79b00aa1
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-23 06:08:15 +0000

    Thu, Feb 23, 2023, 2:08 PM +08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -20,7 +20,7 @@ def main():
         for line in dict_reader:
             #print(f'{type(line["team"])}')
             teams.append(line["team"])
-            print(f"Team:{line["team"]} Rating:{line["rating"]}")
+            print("Team: " + line["team"] + "  |  Rating: " + line["rating"])
 
         for line in dict_reader: # line is an individual dict
             print(line["team"])
