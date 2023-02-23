56319d34980621c2f46e72e6bf02782e2dab5ccc
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-23 13:49:17 +0000

    Thu, Feb 23, 2023, 9:49 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,8 +5,8 @@ def main():
 
     spare = number
     len_number = 0
-    while (spare > 10):
-        spare = counter/10
+    while (spare > 1):
+        spare = spare/10
         len_number += 1
 
     print(len_number)
@@ -17,3 +17,5 @@ def first(number):
     while(number > 10):
         number = (number - number % 10) / 10
 
+
+main()
