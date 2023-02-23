34120a632dd47357ce656ce07fe31c91f76b4def
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-23 09:01:46 +0000

    Thu, Feb 23, 2023, 5:01 PM +08:00

---
+++ sentimental-mario-more/mario.py
@@ -0,0 +1,14 @@
+def main():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0 and n < 9:
+                break
+        except ValueError:
+            print("Not an integer")
+
+    for i in range(n):
+        print(" " * (n-(i+1))  +  "#" * (i+1)  +  " "  +  "#" * (i+1))
+
+
+main()
