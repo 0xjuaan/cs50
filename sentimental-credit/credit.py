4cf0d984bc7c9f426de458835ffb889f3f367cd9
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-23 13:51:28 +0000

    Thu, Feb 23, 2023, 9:51 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,19 +3,22 @@ from cs50 import get_int
 def main():
     number = get_int("Number: ")
 
+
+    # Getting the length of the number
     spare = number
     len_number = 0
     while (spare > 1):
         spare = spare/10
         len_number += 1
 
-    print(len_number)
+    print(first(number))
 
 
 
 def first(number):
     while(number > 10):
         number = (number - number % 10) / 10
+    return int(number)
 
 
 main()
