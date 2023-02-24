5fa73ce7a7d9bf87f975f75cec4c543d110a2461
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-24 04:40:57 +0000

    Fri, Feb 24, 2023, 12:40 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -16,9 +16,13 @@ for i in text:
 
 score = (100 * 0.0588 * (L / W) - 0.296 * (S / W) * 100 - 15.8)
 
+print(L)
+print(W)
+print(S)
+
 if score > 16:
     print("Grade 16+")
 elif score < 1:
     print("Before Grade 1")
 else:
-    print(f"Grade {int(score)}")
+    print(f"Grade {score}")
