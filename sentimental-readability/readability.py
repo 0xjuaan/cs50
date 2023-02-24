d634a19b57f6b880bf38a1cb440dba11dbfcee82
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-24 04:41:59 +0000

    Fri, Feb 24, 2023, 12:41 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,7 +3,7 @@ from cs50 import get_string
 text = get_string("Text: ")
 
 L = 0
-W = 0
+W = 1
 S = 0
 
 for i in text:
@@ -16,13 +16,9 @@ for i in text:
 
 score = (100 * 0.0588 * (L / W) - 0.296 * (S / W) * 100 - 15.8)
 
-print(L)
-print(W)
-print(S)
-
 if score > 16:
     print("Grade 16+")
 elif score < 1:
     print("Before Grade 1")
 else:
-    print(f"Grade {score}")
+    print(f"Grade {round(score)}")
