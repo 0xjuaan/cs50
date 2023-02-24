b3fe665785bf414aa55c8988c1c4c5e9475ec30c
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-24 04:34:50 +0000

    Fri, Feb 24, 2023, 12:34 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,4 +2,18 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
-print(len(text))
+L = 0
+W = 0
+S = 0
+
+for i in text:
+    if i == " ":
+        W += 1
+    elif i in ["!", ".", "?"]:
+        S += 1
+    elif i.isalpha():
+        L +=1
+
+score = int((0.0588 * L - 0.296 * S - 15.8) / W)
+
+print(f"Grade {score}")
