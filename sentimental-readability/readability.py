8dbd5cae80f77dc2aa659e342694dd89bbf787bc
bigjuann <122077337+bigjuann@users.noreply.github.com>
2023-02-24 04:38:57 +0000

    Fri, Feb 24, 2023, 12:38 PM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -14,6 +14,11 @@ for i in text:
     elif i.isalpha():
         L +=1
 
-score = int((0.0588 * L - 0.296 * S - 15.8) / W)
+score = (100 * 0.0588 * (L / W) - 0.296 * (S / W) * 100 - 15.8)
 
-print(f"Grade {score}")
+if score > 16:
+    print("Grade 16+")
+elif score < 1:
+    print("Before Grade 1")
+else:
+    print(f"Grade {int(score)}")
