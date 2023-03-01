f1317513458cba74b3a08b6a7e4fb319e409935b
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-01 02:41:06 +0000

    Wed, Mar 1, 2023, 10:41 AM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,4 +4,12 @@ SELECT city FROM airports; --Finding list of cities
 
 SELECT id FROM airports WHERE city = 'Fiftyville'; --Finding Fiftyville id: 8
 
+--Getting flights from Fiftyville after the incident
+
+SELECT * FROM flights
+WHERE origin_airport_id = 8
+AND year = 2021
+AND month = 7
+AND day >=28;
+
 
