ce24f5a6dd0be75b5a87f89489b0ecb7d19f99d1
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-01 02:36:59 +0000

    Wed, Mar 1, 2023, 10:36 AM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+SELECT city FROM airports; --Finding list of cities
+
+SELECT id FROM airports WHERE city = 'Fiftyville'; --Finding Fiftyville id: 8
+
+
