79a7596471f4d48289b581b1d6696de137f3ceb2
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-02 09:58:57 +0000

    Thu, Mar 2, 2023, 5:58 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -48,7 +48,7 @@ WHERE year = 2021
 AND month = 7
 AND day >=28
 
-AND receiver IN (
+AND receiver IN ( --Their own flight
     SELECT phone_number FROM people WHERE passport_number IN (
     SELECT DISTINCT(passport_number) FROM passengers WHERE flight_id IN (SELECT id FROM flights
     WHERE origin_airport_id = 8
