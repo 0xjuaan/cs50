eb14a09374cd017f16d6dbedb379318d27a5fb9f
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-25 02:55:09 +0000

    Sat, Mar 25, 2023, 10:55 AM +08:00

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 def main():
-    goldy(0)
-
-
-
-
-
-
-
+    goldy(20, 0)
+    print(c)
 
 
 def diff(x,y):
     return y-x
 
-def primes(n):
+def fprimes(n):
     p=[]
     for i in range(n):
         if i == 1:
@@ -31,7 +25,7 @@ def goldy(n, c):
 
     if n < 3:
         return
-    primes = primes(n)
+    primes = fprimes(n)
     diffs = []
     for i in range(n):
         if i in primes:
@@ -39,26 +33,8 @@ def goldy(n, c):
                 if i-j == n:
                     diffs.append(i-j)
 
-    count += 1
+    c += 1
     goldy(diffs(max), count)
 
-
-
-
-
-
-
-for i in range(100):
-    if i == 1:
-        continue
-    if i%2==0 and i != 2:
-        continue
-    elif i%3==0 and i != 3:
-        continue
-    elif i%5==0 and i != 5:
-        continue
-    elif i%7==0 and i != 7:
-        continue
-    print(i)
 
 main()
