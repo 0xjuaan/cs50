0a66cad8b0c0f5deea671a2449a93403aee189cd
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-25 04:05:41 +0000

    Sat, Mar 25, 2023, 12:05 PM +08:00

--- pri.py
+++ pri.py
@@ -3,8 +3,9 @@ status = False
 
 for i in range(1, 20):
     for j in range(1, round(int(i)/2)):
-        if (i % j) == 0:
+        if (i % j == 0 and i != j) == 0:
             status = False
+            
         else:
             status = True
     if status == True:
