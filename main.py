9362ee186250727d3c0556028c6ceb7d509c4808
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-25 02:23:55 +0000

    Sat, Mar 25, 2023, 10:23 AM +08:00

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ def diff(x,y):
     return y-x
 
 
+for i in range(100):
+    if i%2==0 & i!=2:
+        continue
+    elif i%3==0 & i!=3:
+        continue
+    elif i%5==0 & i!=5:
+        continue
+    elif i%7==0 & i!=7:
+        continue
+    print(i)
