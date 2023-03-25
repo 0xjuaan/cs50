13d0abfc4cfc1a68e1cb19497d7695e2122a38fa
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-25 02:25:59 +0000

    Sat, Mar 25, 2023, 10:25 AM +08:00

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ def diff(x,y):
 
 
 for i in range(100):
-    if i%2==0 & i!=2:
+    if i == 1:
         continue
-    elif i%3==0 & i!=3:
+    if i%2==0 and i != 2:
         continue
-    elif i%5==0 & i!=5:
+    elif i%3==0 and i != 3:
         continue
-    elif i%7==0 & i!=7:
+    elif i%5==0 and i != 5:
+        continue
+    elif i%7==0 and i != 7:
         continue
     print(i)
