b23b5950eb4943454ae7dc58829eb849f42e8b11
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-25 02:31:33 +0000

    Sat, Mar 25, 2023, 10:31 AM +08:00

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 def diff(x,y):
     return y-x
 
-primes
+def primes(n):
+    p=[]
+    for i in range(n):
+        if i == 1:
+            continue
+        if i%2==0 and i != 2:
+            continue
+        elif i%3==0 and i != 3:
+            continue
+        elif i%5==0 and i != 5:
+            continue
+        elif i%7==0 and i != 7:
+            continue
+        p.append(i)
+
+def goldy(n):
+    primes = primes(n)
+    
+
 
 for i in range(100):
     if i == 1:
