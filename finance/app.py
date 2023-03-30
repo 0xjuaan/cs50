c3b08ab35b8161cd6a765e3412a657e2157b1756
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-03-30 14:20:36 +0000

    Thu, Mar 30, 2023, 10:20 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -135,7 +135,7 @@ def register():
 
         if password == confirmation and vacant and password and confirmation and username:
             #Hashing password
-            hashed = werkzeug.security.generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+            hashed = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
 
 
             #inputting into database
