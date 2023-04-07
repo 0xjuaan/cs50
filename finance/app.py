d9c1b209e8fa94485c91fe0fd44e2b0c448486b2
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-04-07 07:41:53 +0000

    Fri, Apr 7, 2023, 3:41 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -163,7 +163,7 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
+        return redirect(url_for("/", alert="Welcome back"))
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
