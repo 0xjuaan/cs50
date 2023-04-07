ea99128f093cac856aa247fe9e19ff95850406cc
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-04-07 08:32:48 +0000

    Fri, Apr 7, 2023, 4:32 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -259,6 +259,9 @@ def sell():
         if shares > current_max:
             return apology("Nay mate- you cant sell that many")
 
+        if shares == current_max:
+            db.execute("DELETE FROM stocks WHERE id = ? AND symbol = ?", shares, session["user_id"], symbol)
+
         if shares == 0:
             return redirect(url_for('index', alert="Well guess you don't wanna sell! Here's your portfolio then"))
 
