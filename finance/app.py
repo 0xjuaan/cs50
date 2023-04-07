dbd1cfd23958484eb0ae0bd99b5046d9b6671748
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-04-07 07:23:32 +0000

    Fri, Apr 7, 2023, 3:23 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -217,7 +217,7 @@ def register():
 
             #inputting into database
             db.execute("INSERT INTO users (username, hash, cash) VALUES (?, ?, ?)", username, hashed, 10000)
-            return redirect(url_for('.login', congrats=f"Great! You are officially registered as {username}"))
+            return render_template("login.html", alert=f"Great! You have succesfully registed as {username}")
 
 
         #Invalid inputs
