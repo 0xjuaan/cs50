a8968e43dc675f65002ad22d7f4c77978712e24b
Juan <122077337+juan-selvan@users.noreply.github.com>
2023-04-10 05:40:38 +0000

    Mon, Apr 10, 2023, 1:40 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,12 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 --Finding the descriptions on the day of the crime at humphrey street
-
 SELECT description FROM crime_scene_reports
    ...> WHERE day = 28 and month = 7
    ...> and street = 'Humphrey Street';
+
+--Output:
+--Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
+--| Littering took place at 16:36. No known witnesses.
+
+--Now I'm thinking to check the bakery tapes
